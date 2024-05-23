b5c809b0db4c408adc9b550f030c55668f798529
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-23 09:56:58 +0530

    added the string in sorted order

---
+++ FingerCounter.py
@@ -0,0 +1,37 @@
+import cv2
+import time
+import os
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+folderPath = "FingerImages"
+myList = os.listdir(folderPath)
+myList = sorted(myList, key=lambda x: int(x.split('.')[0]))
+print(myList)
+overlayList = []
+
+pTime = 0
+
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    image = cv2.resize(image, (200, 200))
+    overlayList.append(image)
+
+while True:
+    success, img = cap.read()
+
+    h, w, c = overlayList[0].shape
+    img[0:h, 0:w] = overlayList[0]
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
